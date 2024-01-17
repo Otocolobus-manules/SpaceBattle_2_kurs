35951a4a40906990169c6a1c9caec2fee852ed9d
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-01-17 11:27:52 +0300

    Vector

---
+++ spacebattle/SpaceBattle.Lib/Lab_1/Vector.cs
@@ -0,0 +1,75 @@
+﻿public class Vector
+{
+
+    private int[] coords;
+    public int size;
+
+    public Vector(int[] args)
+    {
+        this.coords = args;
+        this.size = args.Length;
+    }
+
+    public Vector(int Size)
+    {
+        coords = new int[Size];
+        size = Size;
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)	// Сложение векторов 
+    {
+        if (v1.size != v2.size) throw new System.ArgumentException();
+        Vector x = new Vector(v1.size);
+        for (int i = 0; i < v1.size; i++)
+        {
+            x.coords[i] = v1.coords[i] + v2.coords[i];
+        }
+        return x;
+    }
+
+    public static Vector operator -(Vector v1, Vector v2)	// Разность векторов
+    {
+        if (v1.size != v2.size) throw new System.ArgumentException();
+        Vector x = new Vector(v1.size);
+        for (int i = 0; i < v1.size; i++)
+        {
+            x.coords[i] = v1.coords[i] - v2.coords[i];
+        }
+        return x;
+    }
+
+    public static bool operator ==(Vector v1, Vector v2)	// Сравнение на равенство
+    {
+        if (v1.size != v2.size) return false;
+        for (int i = 0; i < v1.size; i++) if (v1.coords[i] != v2.coords[i]) return false;
+        return true;
+    }
+
+    public static bool operator !=(Vector v1, Vector v2)	// Сравнение на неравенство
+    {
+        return !(v1 == v2);
+    }
+
+    public int this[int i]
+    {
+        get => coords[i];
+        set => coords[i] = value;
+    }
+
+    public override bool Equals(object? obj)	// Сравнение объектов
+    {
+        if (obj is not null)
+        {
+            Vector v = (Vector)obj;
+            if (v.size != this.size) return false;
+            for (int i = 0; i < v.size; i++) if (v.coords[i] != this.coords[i]) return false;
+            return true;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()	// Хэш код
+    {
+        return coords.GetHashCode();
+    }
+}
