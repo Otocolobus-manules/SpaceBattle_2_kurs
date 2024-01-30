81412464ecc8194b63916d6922c27f2fc937a0eb
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-01-30 21:46:38 +0300

    добавление TurnCommand тестов

---
+++ spacebattle/SpaceBattle.Lib.Tests/TurnCommandTests/TurnCommandTest.cs
@@ -0,0 +1,54 @@
+﻿namespace DefaultNamespace;
+
+public class TurnCommandTest
+{
+    [Fact]
+    public void Rotate_Test()
+    {
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
+        
+        turnable.SetupGet(t => t.corner).Returns(new Rational(45)).Verifiable();
+        turnable.SetupGet(t => t.delta).Returns(new Rational(90)).Verifiable();
+        
+        TurnCommand turn_command = new TurnCommand(turnable.Object);
+        turn_command.Execute();
+
+        turnable.VerifySet(m => m.corner = new Rational(135), Times.Once);
+    }
+
+    [Fact]
+    public void Corner_Error_Test()
+    {
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
+        
+        turnable.SetupGet(t => t.corner).Throws(new Exception()).Verifiable();
+        turnable.SetupGet(t => t.delta).Returns(new Rational(45)).Verifiable();
+
+        TurnCommand turn_command = new TurnCommand(turnable.Object);
+        Assert.Throws<Exception>(() => turn_command.Execute());
+    }
+    
+    [Fact]
+    public void Delta_Error_Test()
+    {
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
+        turnable.SetupGet(t => t.corner).Returns(new Rational(45)).Verifiable();
+        turnable.SetupGet(t => t.delta).Throws(new Exception()).Verifiable();
+
+        TurnCommand turn_command = new TurnCommand(turnable.Object);
+        Assert.Throws<Exception>(() => turn_command.Execute());
+    }
+    
+    [Fact]
+    public void Error_Set_Corner_Test()
+    {
+        var turnable = new Mock<ITurnable>();
+
+        turnable.SetupGet(t => t.corner).Returns(new Rational(45)).Verifiable();
+        turnable.SetupGet(t => t.delta).Returns(new Rational(45)).Verifiable();
+        turnable.SetupSet(t => t.corner = It.IsAny<Rational>()).Throws(new Exception()).Verifiable();
+
+        var turn_command = new TurnCommand(turnable.Object);
+        Assert.Throws<Exception>(() => turn_command.Execute());
+    }
+}
