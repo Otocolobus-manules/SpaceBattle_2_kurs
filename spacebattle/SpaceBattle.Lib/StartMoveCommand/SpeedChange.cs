6bea9cc6b9162b50efe6074db3c2f5f0b8ac6f75
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-01 19:28:49 +0300

    Добавление IStrategy, SpeedChange

---
+++ spacebattle/SpaceBattle.Lib/StartMoveCommand/SpeedChange.cs
@@ -0,0 +1,16 @@
+﻿public class SpeedChange: ICommand
+{
+    private IUObject _obj;
+    private Vector _velocity;
+
+    public SpeedChange(object obj, Vector velocity)
+    {
+        _obj = (IUObject)obj;
+        _velocity = velocity;
+    }
+
+    public void Execute()
+    {
+        _obj.set_property("velocity", _velocity);
+    }
+}
