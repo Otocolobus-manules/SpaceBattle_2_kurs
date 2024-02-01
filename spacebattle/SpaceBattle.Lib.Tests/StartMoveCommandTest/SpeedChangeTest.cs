d055a1413f8ec22b18430d6db9efb223b21b59c7
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-01 19:50:22 +0300

    SpeedChange тесты

---
+++ spacebattle/SpaceBattle.Lib.Tests/StartMoveCommandTest/SpeedChangeTest.cs
@@ -0,0 +1,21 @@
+﻿public class SpeedChangeTest
+{
+    [Fact]
+    public void Execute_SpeedChange_With_Anothe_With_UIObject()
+    {
+        var dict = new Dictionary<string, object>();
+        var UObject = new Mock<IUObject>();
+        UObject.Setup(x => x.set_property("velocity", It.IsAny<Vector>())).Callback<string, object>((string a, object z) => dict["velocity"] = z);
+        new SpeedChange(UObject.Object, It.IsAny<Vector>()).Execute();
+        UObject.Setup(dict => dict.get_property("velocity")).Returns(dict["velocity"]).Verifiable();
+        Assert.Equal(UObject.Object.get_property("velocity"), dict["velocity"]);
+    }
+    
+    [Fact]
+    public void Execute_SpeedChange_With_Another()
+    {
+        var UObject = new Mock<IUObject>();
+        int[] vector = { 1, 1 };
+        Assert.Throws<System.InvalidCastException>(() => new SpeedChange(new object(), new Vector(vector)).Execute());
+    }
+}
