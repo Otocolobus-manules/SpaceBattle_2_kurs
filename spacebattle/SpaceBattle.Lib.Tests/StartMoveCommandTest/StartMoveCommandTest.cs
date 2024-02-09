eda05d452f0256fa46073fa7378aaca15042a82f
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-09 19:51:12 +0300

    Добавлены тесты для класса StartMoveCommand

---
+++ spacebattle/SpaceBattle.Lib.Tests/StartMoveCommandTest/StartMoveCommandTest.cs
@@ -0,0 +1,91 @@
+﻿public class StartMoveCommandTest
+{
+    [Fact]
+    public void Init_Score_Env()
+    {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        var spaceship = new Mock<IMovable>();
+        
+        var CmdExample = new Mock<ICommand>();
+        CmdExample.Setup(x => x.Execute());
+
+        var ComeBackCommmandStrategy = new Mock<IStartegy>();
+        var ComeBackQueue = new Mock<IStartegy>();
+        var ComeBackSpeedChangeStrategy = new Mock<IStartegy>();
+        ComeBackCommmandStrategy.Setup(x => x.Execute(It.IsAny<object[]>())).Returns(CmdExample.Object);
+        ComeBackQueue.Setup(x => x.Execute()).Returns(new Queue<ICommand>());
+        ComeBackSpeedChangeStrategy.Setup(x => x.Execute(It.IsAny<IUObject>(), It.IsAny<Vector>()));
+
+        var ComeBackMoveCommmandStrategy = new Mock<IStartegy>();
+        var k = new MoveCommand(spaceship.Object);
+        ComeBackMoveCommmandStrategy.Setup(x => x.Execute(It.IsAny<object[]>())).Returns(new MoveCommand(spaceship.Object));
+
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.Set", (object[] args) => ComeBackCommmandStrategy.Object.Execute(args)).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.LongMove", (object[] args) => ComeBackMoveCommmandStrategy.Object.Execute(args)).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Vars.Queue", (object[] args) => ComeBackQueue.Object.Execute()).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.SpeedChange", (object[] args) => ComeBackSpeedChangeStrategy.Object.Execute(args)).Execute();
+    }
+    
+    [Fact]
+    public void StartCommand_Exe()
+    {
+        Init_Score_Env();
+        Vector vector = new Vector(new int[]{1, 1});
+        
+		var UObject = new Mock<IUObject>();
+        UObject.Setup(x => x.set_property("velocity", It.IsAny<Vector>()));
+        
+        var speed_changeable = new Mock<ISpeedChangeable>();
+        speed_changeable.SetupGet(x => x.obj).Returns(UObject.Object).Verifiable();
+        speed_changeable.SetupGet(x => x.speed_change).Returns(vector).Verifiable();
+        
+        var MoveComStart = new Mock<IMoveCommandStartable>();
+        MoveComStart.SetupGet(x => x.velocity).Returns(It.IsAny<Vector>()).Verifiable();
+        MoveComStart.SetupGet(x => x.obj).Returns(UObject.Object).Verifiable();
+        MoveComStart.SetupGet(x => x.queue).Returns(new Queue<ICommand>()).Verifiable();
+
+        var go_round = new StartMoveCommand(MoveComStart.Object);
+        go_round.Execute();
+    }
+    
+    [Fact]
+    public void StartCommand_not_Velocity()
+    {
+        Init_Score_Env();
+        
+        var MoveComStart = new Mock<IMoveCommandStartable>();
+        MoveComStart.SetupGet(x => x.velocity).Throws(new Exception()).Verifiable();
+        MoveComStart.SetupGet(x => x.obj).Returns(It.IsAny<IUObject>).Verifiable();
+        MoveComStart.SetupGet(x => x.queue).Returns(It.IsAny<Queue<ICommand>>).Verifiable();
+
+        Assert.Throws<Exception>(() => new StartMoveCommand(MoveComStart.Object).Execute());
+    }
+    
+    [Fact]
+    public void StartCommand_not_Object()
+    {
+        Init_Score_Env();
+        
+        var MoveComStart = new Mock<IMoveCommandStartable>();
+        MoveComStart.SetupGet(x => x.velocity).Returns(It.IsAny<Vector>()).Verifiable();
+        MoveComStart.SetupGet(x => x.obj).Throws(new Exception()).Verifiable();
+        MoveComStart.SetupGet(x => x.queue).Returns(It.IsAny<Queue<ICommand>>).Verifiable();
+
+        Assert.Throws<Exception>(() => new StartMoveCommand(MoveComStart.Object).Execute());
+    }
+    
+    [Fact]
+    public void StartCommand_not_Queue()
+    {
+        Init_Score_Env();
+        
+        var MoveComStart = new Mock<IMoveCommandStartable>();
+        MoveComStart.SetupGet(x => x.velocity).Returns(It.IsAny<Vector>()).Verifiable();
+        MoveComStart.SetupGet(x => x.obj).Returns(It.IsAny<IUObject>).Verifiable();
+        MoveComStart.SetupGet(x => x.queue).Throws(new Exception()).Verifiable();
+
+        Assert.Throws<Exception>(() => new StartMoveCommand(MoveComStart.Object).Execute());
+    }
+}
