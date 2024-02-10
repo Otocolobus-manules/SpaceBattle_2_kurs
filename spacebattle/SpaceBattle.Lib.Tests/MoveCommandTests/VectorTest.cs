ebf177febafb33972256d863cc1a564413246bcd
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-10 17:04:58 +0300

    Небольшие изменения

--- spacebattle/SpaceBattle.Lib.Tests/MoveCommandTests/VectorTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/MoveCommandTests/VectorTest.cs
@@ -3,19 +3,19 @@
     [Fact]
     public void Create_empty_Vector_Test()
     {
-        Vector v = new Vector(It.IsAny<int>());
+        var v = new Vector(It.IsAny<int>());
     }
 
     [Fact]
     public void Create_fulled_Vector_Test()
     {
-        Vector v = new Vector(new int[] { It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>() });
+        var v = new Vector(new int[] { It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>() });
     }
 
     [Fact]
     public void GetSetItemTest()
     {
-        Vector v = new Vector(new int[] { 3, -2, 1, 4 });
+        var v = new Vector(new int[] { 3, -2, 1, 4 });
         v[1] = 10;
         Assert.Equal(10, v[1]);
     }
@@ -23,26 +23,26 @@
     [Fact]
     public void Sum_Vector()
     {
-        Vector v1 = new Vector(new int[] { 1, 2, 1 });
-        Vector v2 = new Vector(new int[] { 3, -2, 1 });
-        Vector expected = new Vector(new int[] { 4, 0, 2 });
+        var v1 = new Vector(new int[] { 1, 2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1 });
+        var expected = new Vector(new int[] { 4, 0, 2 });
         Assert.True(v1 + v2 == expected);
     }
 
     [Fact]
     public void Sum_Vector_with_different_len()
     {
-        Vector v1 = new Vector(new int[] { 1, 2, 1 });
-        Vector v2 = new Vector(new int[] { 3, -2, 1, 2 });
+        var v1 = new Vector(new int[] { 1, 2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1, 2 });
         Assert.Throws<ArgumentException>(() => v1 + v2);
     }
 
     [Fact]
     public void Eq_NotEq_VectorTest()
     {
-        Vector v1 = new Vector(new int[] { 3, -2, 1 });
-        Vector v2 = new Vector(new int[] { 9, -6, 3 });
-        Vector v3 = new Vector(new int[] { 3, -2, 1 });
+        var v1 = new Vector(new int[] { 3, -2, 1 });
+        var v2 = new Vector(new int[] { 9, -6, 3 });
+        var v3 = new Vector(new int[] { 3, -2, 1 });
         Assert.False(v1 == v2);
         Assert.True(v1 != v2);
         Assert.True(v1 == v3);
@@ -51,54 +51,54 @@
     [Fact]
     public void Eq_different_len()
     {
-        Vector v1 = new Vector(new int[] { 3, -2, 1 });
-        Vector v2 = new Vector(new int[] { 3, -2, 1, 1 });
+        var v1 = new Vector(new int[] { 3, -2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1, 1 });
         Assert.False(v1 == v2);
     }
 
     [Fact]
     public void GetHashCodeTest()
     {
-        Vector v = new Vector(It.IsAny<int>());
+        var v = new Vector(It.IsAny<int>());
         v.GetHashCode();
     }
 
     [Fact]
     public void EqualTest()
     {
-        Vector v = new Vector(new int[] { 3, -2, 1, 4});
-        Vector v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        var v = new Vector(new int[] { 3, -2, 1, 4});
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
         Assert.True(v.Equals(v1));
     }
 
     [Fact]
     public void UnEqualSizeEqualTest()
     {
-        Vector v = new Vector(new int[] { 3, -2, 1 });
-        Vector v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        var v = new Vector(new int[] { 3, -2, 1 });
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
         Assert.True(!v.Equals(v1));
     }
 
     [Fact]
     public void EqualSizeUnequalTest()
     {
-        Vector v = new Vector(new int[] { 3, 2, 1, 4 });
-        Vector v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        var v = new Vector(new int[] { 3, 2, 1, 4 });
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
         Assert.True(!v.Equals(v1));
     }
 
     [Fact]
     public void SumUnequalTest()
     {
-        Vector v2 = new Vector(new int[] { 9, -6, 3 });
-        Vector v3 = new Vector(new int[] { 3, -2, 1, 4 });
+        var v2 = new Vector(new int[] { 9, -6, 3 });
+        var v3 = new Vector(new int[] { 3, -2, 1, 4 });
         Assert.Throws<ArgumentException>(() => v2 + v3);
     }
     
     [Fact]
     public void EqualNullTest()
     {
-        Vector v = new Vector(new int[] { 3, -2, 1 });
+        var v = new Vector(new int[] { 3, -2, 1 });
         Vector? v1 = null;
         Assert.False(v.Equals(v1));
         Assert.False(v.Equals(null));
