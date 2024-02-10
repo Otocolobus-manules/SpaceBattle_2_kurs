d6cbf0a7ef9adaa751a5f42c6595032b8f771bf0
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-10 18:39:55 +0300

    Добавлен класс EndMoveCommand

---
+++ spacebattle/SpaceBattle.Lib/EndMoveCommand/EndMoveCommand.cs
@@ -0,0 +1,17 @@
+﻿public class EndMoveCommand: ICommand
+{
+    private readonly IMoveCommandEndable _endable;
+
+    public EndMoveCommand(IMoveCommandEndable endable)
+    {
+        _endable = endable;
+    }
+
+    public void Execute()
+    {
+        var emptyCommand = Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Commands.EmptyCommand");
+        Hwdtech.IoC.Resolve<IInjectable>("Commands.Inject", _endable.command, emptyCommand);
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Commands.SpeedChange", _endable.obj, new Vector(new int[] {1, 1}));
+        _endable.queue.Dequeue();
+    }
+}
