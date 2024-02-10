798ab56f1c8d5c4caad7683d4e7b3ff468cc8e74
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-10 18:10:14 +0300

    Добавлены тесты BridgeCommand

---
+++ spacebattle/SpaceBattle.Lib.Tests/EndMoveCommandTests/BridgeCommandTest.cs
@@ -0,0 +1,20 @@
+﻿public class BridgeCommandTest
+{
+    [Fact]
+    public void BridgeCommand_Test()
+    {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
+        
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.EmptyCommand", (object[] args) => new EmptyCommand()).Execute();
+        
+        var mockCommand = new Mock<ICommand>();
+        mockCommand.Setup(x => x.Execute()).Verifiable();
+
+        var bridgeCommand = new BridgeCommand(mockCommand.Object);
+        bridgeCommand.Inject(Hwdtech.IoC.Resolve<ICommand>("Commands.EmptyCommand"));
+        bridgeCommand.Execute();
+
+        mockCommand.Verify(m => m.Execute(), Times.Never());
+    }
+}
