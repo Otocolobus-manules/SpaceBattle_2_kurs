fd36f745ce55ce0c0272bd884f05eb9d42b89f09
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-02-10 18:09:53 +0300

    Добавлен класс BridgeCommand

---
+++ spacebattle/SpaceBattle.Lib/EndMoveCommand/BridgeCommand.cs
@@ -0,0 +1,19 @@
+﻿public class BridgeCommand: ICommand, IInjectable
+{
+    private ICommand _command;
+
+    public BridgeCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void Execute()
+    {
+        _command.Execute();
+    }
+
+    public void Inject(ICommand command)
+    {
+        _command = command;
+    }
+}
