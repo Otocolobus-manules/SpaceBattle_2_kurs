9edf23ecc7b2edc0b1fd05a123e1bc017dc1e301
Чушка подзаборная <USoviets@mail.ru>
2024-03-08 07:02:52 +0300

    Добавлены классы StandartChangeThreadCommand и WalkerInQueueWithStopByEmptyQueue

---
+++ spacebattle/SpaceBattle.Lib/ServerThread/WalkerInQueueWithStopByEmptyQueue.cs
@@ -0,0 +1,18 @@
+﻿public class WalkerInQueueWithStopByEmptyQueue : IStartegy
+{
+    Func<object, object> _f_mod;
+    ServerThread _thread;
+
+    public WalkerInQueueWithStopByEmptyQueue(Func<object, object> f_mod, ServerThread thread)
+    {
+        _f_mod = f_mod;
+        _thread = thread;
+    }
+
+    public object Execute(params object[] args)
+    {
+        if (Hwdtech.IoC.Resolve<System.Boolean>("Commands.IsQueueEmpty",
+                args[0])) return Hwdtech.IoC.Resolve<ICommand>("Commands.HardStopCommand", _thread);
+        else return _f_mod(args[0]);
+    }
+}
