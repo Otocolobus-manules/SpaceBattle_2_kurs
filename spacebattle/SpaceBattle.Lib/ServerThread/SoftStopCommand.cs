6a1ce52b781aa567ccbe8fe63a128fb8521379a8
Чушка подзаборная <USoviets@mail.ru>
2024-03-08 07:01:33 +0300

    добавлен класс SoftStopCommand

---
+++ spacebattle/SpaceBattle.Lib/ServerThread/SoftStopCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+
+public class SoftStopCommand : ICommand
+{
+    private BlockingCollection<ICommand> _queue;
+    private readonly ServerThread _thread;
+
+    public SoftStopCommand(ServerThread thread, BlockingCollection<ICommand> queue)
+    {
+        _thread = thread;
+        _queue = queue;
+    }
+
+    public void Execute()
+    {
+        Hwdtech.IoC.Resolve<ICommand>("Commands.ChangeThreadCommand",
+            _thread,
+            Hwdtech.IoC.Resolve<IStartegy>("Commands.WalkerInQueueWithStopByEmptyQueue",
+                Hwdtech.IoC.Resolve<System.Func<object, object>>("Commands.StandartComandPreprocessingFunction"),
+                _thread)).Execute();
+    }
+}
