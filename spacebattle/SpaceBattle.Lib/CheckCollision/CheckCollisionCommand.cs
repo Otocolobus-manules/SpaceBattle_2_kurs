4015cbf8614303583f9a7e655456005f54a70af8
Чушка подзаборная <USoviets@mail.ru>
2024-03-15 09:03:39 +0300

    Добавлен класс CheckCollisionCommand

---
+++ spacebattle/SpaceBattle.Lib/CheckCollision/CheckCollisionCommand.cs
@@ -0,0 +1,28 @@
+﻿using Hwdtech;
+
+public class CheckCollisionCommand : ICommand
+{
+    private readonly IUObject _obj1, _obj2;
+
+    public CheckCollisionCommand(IUObject obj1, IUObject obj2)
+    {
+        _obj1 = obj1;
+        _obj2 = obj2;
+    }
+
+    public void Execute()
+    {
+        var position1 = Hwdtech.IoC.Resolve<List<int>>("Commands.GetProperty", _obj1, "position");
+        var velocity1 = Hwdtech.IoC.Resolve<List<int>>("Commands.GetProperty", _obj1, "velocity");
+        var position2 = Hwdtech.IoC.Resolve<List<int>>("Commands.GetProperty", _obj2, "position");
+        var velocity2 = Hwdtech.IoC.Resolve<List<int>>("Commands.GetProperty", _obj2, "velocity");
+
+        var variations = Hwdtech.IoC.Resolve<List<int>>("Commands.DfVec", position1, position2, velocity1, velocity2);
+
+        var collisionTree = Hwdtech.IoC.Resolve<IDictionary<int, object>>("Commands.CollisionTree");
+
+        variations.ForEach(variation => collisionTree = (IDictionary<int, object>)collisionTree[variation]);
+
+        Hwdtech.IoC.Resolve<ICommand>("Commands.Collision", _obj1, _obj2).Execute();
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib/CheckCollision/FindVariation.cs
@@ -0,0 +1,17 @@
+﻿public class InitFindVariationsCommand : ICommand
+{
+    public void Execute()
+    {
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.DfVec", (object[] args) =>
+        {
+            var positionFirst = (List<int>)args[0];
+            var velocityFirst = (List<int>)args[1];
+            var positionSecond = (List<int>)args[2];
+            var velocitySecond = (List<int>)args[3];
+
+            var variation = positionFirst.Select((value, index) => value - positionSecond[index])
+                .Concat(velocityFirst.Select((value, index) => value - velocitySecond[index])).ToList();
+            return variation;
+        }).Execute();
+    }
+}
