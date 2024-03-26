c55759dd0666f3bd845b41c1d6f6e6e1040b7ff8
Чушка подзаборная <USoviets@mail.ru>
2024-03-26 04:41:47 +0300

    Добавлен класс StartMoveCommand

---
+++ spacebattle/SpaceBattle.Lib/StartMoveCommand/StartMoveCommand.cs
@@ -0,0 +1,17 @@
+﻿using Hwdtech;
+
+public class StartMoveCommand: ICommand
+{
+    private IMoveCommandStartable _startable;
+
+    public StartMoveCommand(IMoveCommandStartable obj)
+    {
+        _startable = obj;
+    }
+
+    public void Execute()
+    {
+        Hwdtech.IoC.Resolve<ICommand>("Commands.SpeedChange", _startable.obj, _startable.velocity);
+        var long_cmd = Hwdtech.IoC.Resolve<ICommand>("Commands.LongMove", _startable.obj);
+    }
+}
