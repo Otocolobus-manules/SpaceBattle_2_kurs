05cc9af361259ece0badaa22abc0da202c32927c
Чушка подзаборная <USoviets@mail.ru>
2024-03-26 05:14:29 +0300

    Добавлены классы StandartChangeThreadCommand, WalkerInQueue, WalkerInQueueWithStopByEmptyQueue

---
+++ spacebattle/SpaceBattle.Lib/ServerThread/StandartChangeThreadCommand.cs
@@ -0,0 +1,16 @@
+﻿public class StandartChangeThreadCommand : ICommand
+{
+    readonly ServerThread _thread;
+    readonly IStartegy _new_strategy;
+
+    public StandartChangeThreadCommand(ServerThread thread, IStartegy new_strategy)
+    {
+        _thread = thread;
+        _new_strategy = new_strategy;
+    }
+
+    public void Execute()
+    {
+        _thread.ChangeMethod(_new_strategy);
+    }
+}
