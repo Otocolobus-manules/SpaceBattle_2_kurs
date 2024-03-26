37521d1ab9ec6af4bd3947f1cb67d4c8c30ea5a1
Чушка подзаборная <USoviets@mail.ru>
2024-03-26 03:59:05 +0300

    Move Command

---
+++ spacebattle/SpaceBattle.Lib.Tests/MoveCommandTest/MoveCommandTest.cs
@@ -0,0 +1,58 @@
+﻿public class MovableTests
+{
+    [Fact]
+    public void MoveCommandStraightLineMovementComplete()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 12, 5 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Vector(new int[] { -7, 3 })).Verifiable();
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        moveCommand.Execute();
+
+        movable.VerifySet(m => m.Position = new Vector(new int[] { 5, 8 }), Times.Once);
+        movable.VerifyAll();
+    }
+
+    [Fact]
+    public void MoveCommandCantReadPosition()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+    }
+
+    [Fact]
+    public void MoveCommandCantReadVelocity()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 1, 4 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+    }
+
+    [Fact]
+    public void MoveCommandCantSetPosition()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 1, 4 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Vector(new int[] { 1, 2 })).Verifiable();
+        movable.SetupSet(m => m.Position = It.IsAny<Vector>()).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+        movable.VerifyAll();
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib.Tests/MoveCommandTest/VectorTest.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System;
+using System.Diagnostics;
+
+public class VectorTest
+{
+    [Fact]
+    public void Create_empty_Vector_Test()
+    {
+        var v = new Vector(It.IsAny<int>());
+    }
+
+    [Fact]
+    public void Create_fulled_Vector_Test()
+    {
+        var v = new Vector(new int[] { It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>() });
+    }
+
+    [Fact]
+    public void GetSetItemTest()
+    {
+        var v = new Vector(new int[] { 3, -2, 1, 4 });
+        v[1] = 10;
+        Assert.Equal(10, v[1]);
+    }
+
+    [Fact]
+    public void Sum_Vector()
+    {
+        var v1 = new Vector(new int[] { 1, 2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1 });
+        var expected = new Vector(new int[] { 4, 0, 2 });
+        Assert.True(v1 + v2 == expected);
+    }
+
+    [Fact]
+    public void Sum_Vector_with_different_len()
+    {
+        var v1 = new Vector(new int[] { 1, 2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1, 2 });
+        Assert.Throws<ArgumentException>(() => v1 + v2);
+    }
+
+    [Fact]
+    public void Eq_NotEq_VectorTest()
+    {
+        var v1 = new Vector(new int[] { 3, -2, 1 });
+        var v2 = new Vector(new int[] { 9, -6, 3 });
+        var v3 = new Vector(new int[] { 3, -2, 1 });
+        Assert.False(v1 == v2);
+        Assert.True(v1 != v2);
+        Assert.True(v1 == v3);
+    }
+
+    [Fact]
+    public void Eq_different_len()
+    {
+        var v1 = new Vector(new int[] { 3, -2, 1 });
+        var v2 = new Vector(new int[] { 3, -2, 1, 1 });
+        Assert.False(v1 == v2);
+    }
+
+    [Fact]
+    public void GetHashCodeTest()
+    {
+        var v = new Vector(It.IsAny<int>());
+        v.GetHashCode();
+    }
+
+    [Fact]
+    public void EqualTest()
+    {
+        var v = new Vector(new int[] { 3, -2, 1, 4});
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        Assert.True(v.Equals(v1));
+    }
+
+    [Fact]
+    public void UnEqualSizeEqualTest()
+    {
+        var v = new Vector(new int[] { 3, -2, 1 });
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        Assert.True(!v.Equals(v1));
+    }
+
+    [Fact]
+    public void EqualSizeUnequalTest()
+    {
+        var v = new Vector(new int[] { 3, 2, 1, 4 });
+        var v1 = new Vector(new int[] { 3, -2, 1, 4 });
+        Assert.True(!v.Equals(v1));
+    }
+
+    [Fact]
+    public void SumUnequalTest()
+    {
+        var v2 = new Vector(new int[] { 9, -6, 3 });
+        var v3 = new Vector(new int[] { 3, -2, 1, 4 });
+        Assert.Throws<ArgumentException>(() => v2 + v3);
+    }
+    
+
+    [Fact]
+    public void EqualNullTest()
+    {
+        var v = new Vector(new int[] { 3, -2, 1 });
+        Vector? v1 = null;
+        Assert.False(v.Equals(v1));
+        Assert.False(v.Equals(null));
+    }
+}
