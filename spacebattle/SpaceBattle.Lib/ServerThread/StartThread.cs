a0f8f3b618382877fa74c7461980a709352d3ab5
Чушка подзаборная <USoviets@mail.ru>
2024-03-26 05:13:51 +0300

    Добавлен класс StartThread

---
+++ spacebattle/SpaceBattle.Lib/ServerThread/StartThread.cs
@@ -0,0 +1,14 @@
+﻿public class StartThread : ICommand
+{
+    readonly ServerThread _server_thread;
+
+    public StartThread(ServerThread thread)
+    {
+        _server_thread = thread;
+    }
+
+    public void Execute()
+    {
+        ((Thread)_server_thread.Execute()).Start();
+    }
+}
