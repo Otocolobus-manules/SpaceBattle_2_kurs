30dda79e033461702cb0eaecd3219d021c26e766
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-04-18 20:23:45 +0300

    Добавлен класс MacroCommandStrategy

---
+++ spacebattle/SpaceBattle.Lib/MacroCommand/MacroCommandStrategy.cs
@@ -0,0 +1,12 @@
+﻿public class MacroCommandStrategy: IStartegy
+{
+    public object Execute(params object[] args)
+    {
+        var commands = new List<ICommand>();
+        args.ToList().ForEach(cmd =>
+        {
+            commands.Add(Hwdtech.IoC.Resolve<ICommand>((string)cmd));
+        });
+        return Hwdtech.IoC.Resolve<ICommand>("Commands.MacroCommand", commands.ToArray());
+    }
+}
