65dfc1d48ed860f42d446f6b5293cfcc13f08a70
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-04-18 20:42:27 +0300

    Добавлен класс GameMessagePreprocessingStrategy

---
+++ spacebattle/SpaceBattle.Lib/MessageProcessing/GameMessagePreprocessingStrategy.cs
@@ -0,0 +1,12 @@
+﻿public class GameCommandMessagePreprocessingStrategy: IStartegy
+{
+    public GameCommandMessagePreprocessingStrategy(){}
+    
+    public object Execute(params object[] args)
+    {
+        return new Dictionary<string, object>(){{ "game_id", Hwdtech.IoC.Resolve<System.Object>("Get.GameId.FromGameCommandMessage", args[0])},
+            { "command", Hwdtech.IoC.Resolve<System.Object>("Get.CommandName.FromGameCommandMessage", args[0]) },
+            { "args", Hwdtech.IoC.Resolve<System.Object>("Get.Args.FromGameCommandMessage", args[0]) }};
+    }
+    
+}
