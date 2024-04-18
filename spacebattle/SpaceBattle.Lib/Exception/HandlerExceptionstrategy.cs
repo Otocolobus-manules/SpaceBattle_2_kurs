5ae7d4fe23cb5c9bb16bdddb03b8acbce2d24c32
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-04-18 21:37:12 +0300

    Добавлен класс HandlerExceptionstrategy

---
+++ spacebattle/SpaceBattle.Lib/Exception/HandlerExceptionstrategy.cs
@@ -0,0 +1,21 @@
+﻿public class HandlerExceptionStrategy: IStartegy
+{
+    public object Execute(params object[] args)
+    {
+        var _keyCmd = args[0].ToString();
+        var _keyException = args[1].ToString();
+
+        var tree = Hwdtech.IoC.Resolve<Dictionary<string, IExceptionHandler>>("ExceptionHandler.Tree");
+
+        if (tree.ContainsKey(_keyCmd + _keyException))
+        {
+            var handlerCmdExc = tree[_keyCmd + _keyException];
+            return handlerCmdExc;
+        }
+
+        else
+        {
+            return Hwdtech.IoC.Resolve<IExceptionHandler>("DefaultExceptionHandler");
+        }
+    }
+}
