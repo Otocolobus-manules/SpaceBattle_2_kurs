3393316de41f1a0d9a33332b54256255d238b7e5
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-04-18 20:42:06 +0300

    Добавлен класс ObjectIdContainer

---
+++ spacebattle/SpaceBattle.Lib/MessageProcessing/ObjectIdContainer.cs
@@ -0,0 +1,17 @@
+﻿public class ObjectIdContainer: IStartegy
+{
+    private Func<object, object, object> func_of_search;
+    private object container;
+    
+    public ObjectIdContainer(Func<object, object, object> func_of_search, object container)
+    {
+        this.container = container;
+        this.func_of_search = func_of_search;
+    }
+    
+    public object Execute(params object[] arg)
+    {
+        return func_of_search(arg[0], container);
+    }
+    
+}
