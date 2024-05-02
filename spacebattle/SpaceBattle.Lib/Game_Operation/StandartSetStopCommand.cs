d32aa7c2e2ff4637dc5118b0ba638813ac6cefe0
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-02 17:36:12 +0300

    Добавлен класс StandartSetStopCommand

---
+++ spacebattle/SpaceBattle.Lib/Game_Operation/StandartSetStopCommand.cs
@@ -0,0 +1,13 @@
+﻿public class StandartSetScopeCommand: ICommand
+{
+    object _scope;
+    public StandartSetScopeCommand(object scope)
+    {
+        _scope = scope;
+    }
+
+    public void Execute()
+    {
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", _scope).Execute();
+    }
+}
