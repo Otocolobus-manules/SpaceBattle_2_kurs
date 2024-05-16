478e64f7a43699da720152a141c281489e44cdcd
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-16 18:13:44 +0300

    Добавлен класс IterGeneratorCommand

---
+++ spacebattle/SpaceBattle.Lib/InitGame/IterGeneratorCommand.cs
@@ -0,0 +1,16 @@
+﻿public class IterGeneratorCommand: ICommand
+{
+    object _iterator;
+    object _stop;
+    
+    public IterGeneratorCommand(object iterator, object stop)
+    {
+        _iterator = iterator;
+        _stop = stop;
+    }
+    
+    public void Execute()
+    {
+        Hwdtech.IoC.Resolve<ICommand>("IterGeneratorCommand.Run", _iterator, _stop, this).Execute();
+    }
+}
