d7e52183f3bdd9bb5e6c99a6cd3df681d31d5980
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-16 18:13:30 +0300

    Добавлен класс Iterator

---
+++ spacebattle/SpaceBattle.Lib/InitGame/Iterator.cs
@@ -0,0 +1,16 @@
+﻿public class Iterator
+{
+    public object _position { get; protected set; }
+    object _step_function;
+    
+    public Iterator(object start, object step_function){
+        _position = start;
+        _step_function = step_function;
+    }
+    
+    public object next(){
+
+        _position = Hwdtech.IoC.Resolve<System.Object>("Iterator.Step", _step_function, _position);
+        return _position;
+    }
+}
