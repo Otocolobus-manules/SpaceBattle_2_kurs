710fc04718090988e4ecb6247f7485a2296bc09b
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-22 22:03:25 +0300

    Добавлен класс CodeGenerationStrategy

---
+++ spacebattle/SpaceBattle.Lib/GeneratingAdapters/CodeGenerationStrategy.cs
@@ -0,0 +1,14 @@
+﻿public class CodeGenerationStrategy: IStartegy
+{
+    object _template;
+    
+    public CodeGenerationStrategy(object template)
+    {
+        _template = template;
+    }
+    
+    public object Execute(params object[] args)
+    {
+        return Hwdtech.IoC.Resolve<IStartegy>("TemplateBuilderStrategy", _template).Execute(Hwdtech.IoC.Resolve<System.Object>("GenerateCode.Modules.Preprocessing", args));
+    }
+}
