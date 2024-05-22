52c8a66fe90bc479a9f53f6e8825ef54624d5c8a
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-22 22:05:15 +0300

    Добавлен класс SetDefinitionInCodeGenerationStrategy

---
+++ spacebattle/SpaceBattle.Lib/GeneratingAdapters/SetDefinitionInCodeGenerationStrategy.cs
@@ -0,0 +1,14 @@
+﻿public class SetDefinitionInCodeGenerationStrategy: IStartegy
+{
+    object _definition;
+    
+    public SetDefinitionInCodeGenerationStrategy(object definition)
+    {
+        _definition = definition;
+    }
+    
+    public object Execute(params object[] args)
+    {
+        return Hwdtech.IoC.Resolve<System.Object>("Add.Definition.To.Modules", _definition, args[0]);
+    }
+}
