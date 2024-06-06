8a4d03b586ec716cfbc39cfe0d10d8dfb479cd7a
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-06-06 22:21:38 +0300

    Добавлен класс ShipPositionIterator

---
+++ spacebattle/SpaceBattle.Lib/InitGame/ShipPositionIterator.cs
@@ -0,0 +1,20 @@
+﻿public class ShipPositionIterator: IEnumerator<object>  
+{
+    private IList<Vector> _coord;
+    private int _coordind;
+
+    public ShipPositionIterator()
+    {
+        _coordind = 0;
+        _coord = Hwdtech.IoC.Resolve<List<Vector>>("ShipPositions");
+    }
+
+    public object Current => _coord[_coordind];
+
+    public bool MoveNext() => ++_coordind < _coord.Count;
+
+    public void Reset() => _coordind = 0;
+
+    public void Dispose() => throw new System.NotImplementedException();                                          
+
+}
