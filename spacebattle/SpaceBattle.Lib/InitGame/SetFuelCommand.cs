799d78342830e0a604beef5a24a478ea208ffd63
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-06-06 22:19:09 +0300

    Добавлен класс SetFuelCommand.cs

---
+++ spacebattle/SpaceBattle.Lib/InitGame/SetFuelCommand.cs
@@ -0,0 +1,18 @@
+﻿public class SetFuelCommand: ICommand  
+{
+    private IEnumerable<IUObject> _gameobj;
+    private double _fuel;
+
+    public SetFuelCommand(IEnumerable<IUObject> gameobj, double fuel)
+    {
+        _gameobj = gameobj;
+        _fuel = fuel;
+    }
+
+    public void Execute()
+    {
+        _gameobj.ToList().ForEach(
+            x => Hwdtech.IoC.Resolve<ICommand>("UObjectSetProperty", x, "Fuel", _fuel).Execute()
+        );
+    }
+}
