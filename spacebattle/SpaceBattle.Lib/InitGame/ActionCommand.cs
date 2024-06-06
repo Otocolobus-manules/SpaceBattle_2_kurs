889866ea848d0a7344757c90a2d1b7754464f65d
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-06-06 22:19:30 +0300

    Добавлены тесты

---
+++ spacebattle/SpaceBattle.Lib/InitGame/ActionCommand.cs
@@ -0,0 +1,11 @@
+﻿public class ActionCommand: ICommand
+{
+    private Action _action;
+    
+    public ActionCommand(Action action)
+    {
+        _action = action;
+    }
+
+    public void Execute() => _action();
+}
